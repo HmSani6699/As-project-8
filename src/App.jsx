42ade18283a1b7c29dfe95a306516842d654cd94
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-03-06 16:17:52 +0600

    added new text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
 import "./App.css";
 import Blogs from "./component/Blogs/Blogs";
 import Header from "./component/Header/Header";
+import TestGetMultipulValue from "./component/TestComponent/TestGetMultipulValue";
 
 function App() {
+  const [open, setOpen] = useState("old");
+  const [myDetails, setMyDetails] = useState({
+    name: "",
+    age: "",
+    address: "",
+    gram: "",
+  });
   return (
     <div className="App">
       <Header></Header>
-      <Blogs></Blogs>
+      {/* <Blogs></Blogs> */}
+
+      {/* ======================= */}
+
+      <button onClick={() => setOpen("old")}>Old contant</button>
+      <button onClick={() => setOpen("new")}>New contant</button>
+
+      {open === "old" ? (
+        <div>
+          <h2 style={{ fontSize: "25px", fontWeight: "bold" }}>
+            Test Old contant
+          </h2>
+        </div>
+      ) : open === "new" ? (
+        <div>
+          <h2 style={{ fontSize: "25px", fontWeight: "bold" }}>
+            Test New contant
+          </h2>
+          <TestGetMultipulValue
+            myDetails={myDetails}
+            setMyDetails={setMyDetails}
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
---
+++ src/component/TestComponent/TestGetMultipulValue.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TestGetMultipulValue = ({ myDetails, myDetails }) => {
+  return (
+    <div>
+      <h2 style={{ fontSize: "25px", fontWeight: "bold" }}>
+        Added new contant
+      </h2>
+    </div>
+  );
+};
+
+export default TestGetMultipulValue;
